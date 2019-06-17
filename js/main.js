6fea5a4f647e55cf329e594f135de75ab1b698c6
Mushroomy <mushroomy.work@gmail.com>
2019-06-17 09:40:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,13 +27,13 @@ shuffleArray(photosArray);
 var likesArray = makeNumArray(15, 200);
 shuffleArray(likesArray);
 var commentsList = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -59,7 +59,7 @@ var createPictures = function (picture) {
   pictureElement.querySelector('.picture__likes').textContent = picture.likes;
   pictureElement.querySelector('.picture__comments').textContent = picture.comments;
   return pictureElement;
-}
+};
 var picturesCont = document.querySelector('.pictures');
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < picturesList.length; i++) {
